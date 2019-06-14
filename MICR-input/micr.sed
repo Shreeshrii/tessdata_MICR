262b33c2adcd7086fcd7d3a93adf43447e29d600
Shree <shreeshrii@gmail.com>
2019-06-14 10:18:32 +0000

    Initial commit for MICR legacy

---
+++ MICR-input/micr.sed
@@ -0,0 +1,8 @@
+s/A/⑆/g
+s/B/⑇/g
+s/C/⑈/g
+s/D/⑉/g
+s/a/⑆/g
+s/b/⑇/g
+s/c/⑈/g
+s/d/⑉/g
---
+++ MICR.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#./MICR-input/eng.MICR.training_text  uses Unicode codepoints
+# MICR Encoding font uses A B C D instead of Unicode codepoints
+# use micr.sed to convert MICR Encoding box files to use Unicode codepoints
+# eng.mcr.exp0.tif/box based on file by Hunter Beanland (hunter @ beanland.net.au)
+
+time ~/tesseract/src/training/tesstrain.sh \
+  --fonts_dir ~/.fonts \
+  --lang eng \
+  --langdata_dir ./MICR-input/langdata \
+  --tessdata_dir ~/tessdata_best \
+  --fontlist \
+  "MICR Encoding" \
+  --training_text ./MICR-input/langdata/eng/eng.MICR.training_text \
+  --workspace_dir ~/tmp \
+  --save_box_tiff  \
+  --overwrite \
+  --output_dir ./MICR-legacy
+
+rm ./MICR-legacy/eng.traineddata
+sed -i -f ./MICR-input/micr.sed ./MICR-legacy/eng.MICR_Encoding.exp0.box
+mv ./MICR-legacy/eng.MICR_Encoding.exp0.box ./MICR-input/
+mv ./MICR-legacy/eng.MICR_Encoding.exp0.tif ./MICR-input/
+
+time ~/tesseract/src/training/tesstrain.sh \
+  --fonts_dir ~/.fonts \
+  --lang eng \
+  --langdata_dir ./MICR-input/langdata \
+  --tessdata_dir ~/tessdata_best \
+  --fontlist \
+  "Arial Unicode MS" \
+  --training_text ./MICR-input/langdata/eng/eng.MICRsymbols.training_text \
+  --workspace_dir ~/tmp \
+  --my_boxtiff_dir ./MICR-input \
+  --save_box_tiff  \
+  --overwrite \
+  --output_dir ./MICR-legacy
+
+mv ./MICR-legacy/eng.traineddata ./MICR-legacy/MICR.traineddata
+mv ./MICR-legacy/eng.*.box ./MICR-input/
+mv ./MICR-legacy/eng.*.tif ./MICR-input/
+
+tesseract ./MICR-input/test/MICR-test.png  ./MICR-input/test/MICR-test -l MICR --tessdata-dir ./MICR-legacy  -c page_separator="" 
+diff ./MICR-input/test/MICR-test.txt ./MICR-input/test/MICR-test.gt.txt 
